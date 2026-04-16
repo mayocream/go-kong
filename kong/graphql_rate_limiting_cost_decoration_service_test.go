c75a78b4ecb14ab060fcb97896029ba49db3b995
shivaygupta <shivay.gupta@konghq.com>
2026-04-16 12:17:59 +0530

    feat: 'service' field added for GraphqlRateLimitingCostDecoration (#608)
    * feat: Added service field for GraphqlRateLimitingCostDecoration and respective CRUD methods
    
    * fix: fixed linter issue
    
    * feat: Added service config in zz_generated.deepcopy.go for GraphqlRateLimitingCostDecoration
    
    * feat: Added changes for review comments and updated PUT endpoint
    
    * feat: Added support Deleteforservice func
    
    * feat: Updated test for DeleteforService func
    
    * feat: Updated runWhen condition for test to support latest patch
    
    * test: updated test condition to include 3.14.0.2
    
    * chore: removed todo comment.

--- kong/graphql_rate_limiting_cost_decoration_service_test.go
+++ kong/graphql_rate_limiting_cost_decoration_service_test.go
@@ -96,3 +96,116 @@ func TestGraphqlRateLimitingCostDecorationService(t *testing.T) {
 		require.Nil(t, createdDeco)
 	})
 }
+
+func TestGraphqlRateLimitingCostDecorationServiceForService(t *testing.T) {
+	RunWhenEnterprise(t, "=3.4.3.25 || =3.10.0.10 || =3.11.0.9 || =3.12.0.5 || =3.13.0.3 || >=3.14.0.2",
+		RequiredFeatures{})
+	RunWhenDBMode(t, "postgres")
+
+	client, err := NewTestClient(nil, nil)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	// Create a service to associate cost decorations with.
+	service := &Service{
+		Name: String("test-graphql-service2"),
+		Host: String("mockbin.org"),
+	}
+	service, err = client.Services.Create(defaultCtx, service)
+	require.NoError(t, err)
+	require.NotNil(t, service)
+	defer func() {
+		_ = client.Services.Delete(defaultCtx, service.ID)
+	}()
+
+	// Enable the graphql-rate-limiting-advanced plugin on the service.
+	plugin := &Plugin{
+		Name: String("graphql-rate-limiting-advanced"),
+		Service: &Service{
+			ID: service.ID,
+		},
+		Config: Configuration{
+			"limit":       []interface{}{float64(10)},
+			"window_size": []interface{}{float64(60)},
+			"strategy":    "cluster",
+			"sync_rate":   float64(-1),
+		},
+	}
+	plugin, err = client.Plugins.Create(defaultCtx, plugin)
+	require.NoError(t, err)
+	require.NotNil(t, plugin)
+	defer func() {
+		_ = client.Plugins.Delete(defaultCtx, plugin.ID)
+	}()
+
+	t.Run("CreateForService without ID (POST) create/get/update/delete cycle", func(t *testing.T) {
+		deco := &GraphqlRateLimitingCostDecoration{
+			Service:      service,
+			TypePath:     String("Vehicle.name"),
+			AddConstant:  Float64(8),
+			MulArguments: []*string{String("first")},
+		}
+
+		createdDeco, err := client.GraphqlRateLimitingCostDecorations.CreateForService(defaultCtx, deco)
+		require.NoError(t, err)
+		require.NotNil(t, createdDeco)
+
+		deco, err = client.GraphqlRateLimitingCostDecorations.Get(defaultCtx, createdDeco.ID)
+		require.NoError(t, err)
+		require.NotNil(t, deco)
+
+		deco.Service = service
+		deco.TypePath = String("car.designation")
+		deco, err = client.GraphqlRateLimitingCostDecorations.UpdateForService(defaultCtx, deco)
+		require.NoError(t, err)
+		require.NotNil(t, deco)
+		require.Equal(t, "car.designation", *deco.TypePath)
+
+		err = client.GraphqlRateLimitingCostDecorations.DeleteForService(defaultCtx, createdDeco)
+		require.NoError(t, err)
+	})
+
+	t.Run("CreateForService with ID (PUT) create/get/update/delete cycle", func(t *testing.T) {
+		id := uuid.NewString()
+		deco := &GraphqlRateLimitingCostDecoration{
+			ID:           String(id),
+			Service:      service,
+			TypePath:     String("Vehicle.name"),
+			AddConstant:  Float64(8),
+			MulArguments: []*string{String("first")},
+		}
+
+		createdDeco, err := client.GraphqlRateLimitingCostDecorations.CreateForService(defaultCtx, deco)
+		require.NoError(t, err)
+		require.NotNil(t, createdDeco)
+		require.Equal(t, id, *createdDeco.ID)
+
+		deco, err = client.GraphqlRateLimitingCostDecorations.Get(defaultCtx, createdDeco.ID)
+		require.NoError(t, err)
+		require.NotNil(t, deco)
+		require.Equal(t, id, *deco.ID)
+
+		deco.Service = service
+		deco.TypePath = String("car.designation")
+		deco, err = client.GraphqlRateLimitingCostDecorations.UpdateForService(defaultCtx, deco)
+		require.NoError(t, err)
+		require.NotNil(t, deco)
+		require.Equal(t, id, *deco.ID)
+		require.Equal(t, "car.designation", *deco.TypePath)
+
+		err = client.GraphqlRateLimitingCostDecorations.DeleteForService(defaultCtx, createdDeco)
+		require.NoError(t, err)
+	})
+
+	t.Run("Can't CreateForService Cost Decoration without service", func(t *testing.T) {
+		deco := &GraphqlRateLimitingCostDecoration{
+			TypePath:     String("car.name"),
+			AddConstant:  Float64(8),
+			MulArguments: []*string{String("first")},
+		}
+
+		createdDeco, err := client.GraphqlRateLimitingCostDecorations.CreateForService(defaultCtx, deco)
+		require.Error(t, err, "cannot create a cost decoration for a service without name or ID")
+		require.Nil(t, createdDeco)
+	})
+}
